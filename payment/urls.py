473e6896385e096160e0c96e805e1ca143c6330c
Bruno Oyuko <84352218+oyuko-bruno@users.noreply.github.com>
2024-07-08 12:16:28 -0700

    Update urls.py

--- payment/urls.py
+++ payment/urls.py
@@ -1,8 +1,14 @@
 from django.urls import path
-from . import views 
+
+from stkpush import views
+
+
+app_name = "stkpush"
 
 urlpatterns = [
-    path('mpesa_stk_push/', views.mpesa_stk_push, name='mpesa_stk_push'),
-    path('mpesa_confirmation/', views.mpesa_confirmation, name='mpesa_confirmation'),
-    path('mpesa_validation/', views.mpesa_validation, name='mpesa_validation'),
+    path('', views.home, name="home"),
+    path('token', views.token, name='token'),
+    path('pay', views.pay, name='pay'),
+    path('stk', views.stk, name="stk")
+
 ]
